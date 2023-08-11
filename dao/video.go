57e2144f8782704a517b4d6fb9b46037dc5d84b4
快快学习 <1993584108@qq.com>
2023-08-11 13:16:28 +0800

    fix 修复video list查询接口

--- dao/video.go
+++ dao/video.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"gorm.io/gorm"
 	"main/model"
 	"time"
 )
@@ -9,19 +10,21 @@ import (
 func GetVideoList(latestTime time.Time, maxCount int, userID uint) ([]*model.Video, error) {
 	var videoList []*model.Video
 
-	err := DB.Preload("Author").
+	err := DB.Preload("Author", func(db *gorm.DB) *gorm.DB {
+		return db.Table("users u").Select("u.*, " +
+			"COUNT(DISTINCT v.id) work_count," +
+			"COUNT(DISTINCT lv.id) total_favorited," +
+			"COUNT(DISTINCT lu.id) favorite_count").
+			Joins("LEFT JOIN videos v ON u.id = v.author_id").
+			Joins("LEFT JOIN likes lv ON v.id = lv.video_id").
+			Joins("LEFT JOIN likes lu ON u.id = lu.id").
+			Group("u.id")
+	}).
 		Select("videos.*, "+
 			"(SELECT COUNT(*) FROM likes l WHERE videos.id = l.video_id) AS favorite_count, "+
 			"(SELECT COUNT(*) FROM comments c WHERE videos.id = c.video_id) AS comment_count, "+
 			"EXISTS(SELECT * FROM likes l WHERE videos.id = l.video_id AND l.user_id = ?) AS is_favorite",
 			userID).
-		Joins("LEFT JOIN (SELECT u.*, COUNT(DISTINCT v.id) AS work_count, "+
-			"COUNT(DISTINCT lv.id) AS total_favorited, COUNT(DISTINCT lu.id) AS favorite_count "+
-			"FROM users u "+
-			"LEFT JOIN videos v ON u.id = v.author_id "+
-			"LEFT JOIN likes lv ON v.id = lv.video_id "+
-			"LEFT JOIN likes lu ON u.id = lu.user_id "+
-			"GROUP BY u.id) users ON users.id = videos.author_id").
 		Where("videos.created_at <= ?", latestTime).
 		Order("videos.created_at").
 		Limit(maxCount).
--- sql/user.sql
+++ sql/user.sql
@@ -1,11 +1,21 @@
 # 查询用户信息及作品数、获赞数、喜欢数
-SELECT
-    u.*,
-    COUNT(DISTINCT v.id) work_count,
-    COUNT(DISTINCT lv.id) total_favorited,
-    COUNT(DISTINCT lu.id) favorite_count
+SELECT u.*,
+       COUNT(DISTINCT v.id)  work_count,
+       COUNT(DISTINCT lv.id) total_favorited,
+       COUNT(DISTINCT lu.id) favorite_count
 FROM users u
-LEFT JOIN videos v ON u.id = v.author_id
-LEFT JOIN likes lv ON v.id = lv.video_id
-LEFT JOIN likes lu on u.id = lu.user_id
+         LEFT JOIN videos v ON u.id = v.author_id
+         LEFT JOIN likes lv ON v.id = lv.video_id
+         LEFT JOIN likes lu on u.id = lu.user_id
 WHERE u.id = 15;
+
+SELECT u.*,
+       COUNT(DISTINCT v.id)  work_count,
+       COUNT(DISTINCT lv.id) total_favorited,
+       COUNT(DISTINCT lu.id) favorite_count
+FROM users u
+         LEFT JOIN videos v ON u.id = v.author_id
+         LEFT JOIN likes lv ON v.id = lv.video_id
+         LEFT JOIN likes lu ON u.id = lu.id
+WHERE `u`.`id` IN (1, 5)
+GROUP BY `u`.`id`;
